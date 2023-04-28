ca2eb5b7f8e02e337bd154b803f42c8f65a677d0
joão <joaopedrot1912@gmail.com>
2023-04-28 11:04:31 -0300

    App atualizado
    Iteração e algumas regras aprendidas

--- src/App.js
+++ src/App.js
@@ -1,35 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
 
 
 class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      name: "joao pedro"
-    }
+  state = {
+    posts: [
+      {
+        id: 1,
+        title: "O titulo 1",
+        body: "O corpo 1"
+      },
+      {
+        id: 2,
+        title: "O titulo 2",
+        body: "O corpo 2"
+      },
+      {
+        id: 3,
+        title: "O titulo 3",
+        body: "O corpo 3"
+      }
+    ]
   }
 
   render() {
-    const {name} = this.state;
+    const { posts } = this.state;
 
     return(
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p onClick={()=> {alert("TESTANDO O EVENTO")}}>
-            {name}
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {posts.map( post => (
+        <div key={post.id}>
+          <h1>{post.title}</h1>
+          <p>{post.body}</p>
+        </div>
+        ) )}
       </div>
     );
   }
